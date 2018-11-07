5170ff053b2ce18f6c13014b25666e691e945d08
Devin Hughes <devin@jd2.com>
2018-11-07 05:02:21 -0500

    Initial commit of igh ethercat modules

---
+++ drivers/ethercat/config.h
@@ -0,0 +1,11 @@
+/* Max. number of Ethernet devices per master */
+#define EC_MAX_NUM_DEVICES 1
+
+/* Output to syslog in RT context */
+#define EC_RT_SYSLOG 1
+
+/* Assign SII to PDI */
+#define EC_SII_ASSIGN 1
+
+/* Version number of package */
+#define VERSION "1.5.2"
